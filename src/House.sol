baac07c37301cd5d03e7645fc930013cca1f71ea
indigo <6822158+ind-igo@users.noreply.github.com>
2022-04-27 01:31:35 -0400

    init

---
+++ src/House.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GFYL
+pragma solidity ^0.8.10;
+
+import {YieldSplitter} from "./base/YieldSplitter.sol";
+
+contract House {
+    uint256 public buyIn;
+
+    uint256 
+
+    constructor() {}
+
+    function deposit(uint256 amount_) external {
+
+    }
+
+    function getPayout() public view returns (uint256) {
+        return 0;
+    }
+
+    function _splitYield() internal {}
+}
